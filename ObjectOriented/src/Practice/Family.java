2d02b280908d260fed750c4465908363c3bf6805
partheev-boora <partheev.boora@kpipartners.com>
2022-09-27 12:38:34 +0530

    commit interfaces

---
+++ ObjectOriented/src/Practice/Family.java
@@ -0,0 +1,5 @@
+package Practice;
+
+public interface Family {
+    int noOfMembers();
+}
---
+++ ObjectOriented/src/Practice/Human.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class Human implements Friends,Family{
+
+	@Override
+	public int noOfMembers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int noOfFrnds() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+    
+}
