0b0d6c62eba1298b6568249ecbed491cf8e34541
partheev-boora <partheev.boora@kpipartners.com>
2022-09-27 09:41:50 +0530

    commit

--- ObjectOriented/src/Practice/Rectangle.java
+++ ObjectOriented/src/Practice/Rectangle.java
@@ -2,11 +2,9 @@ package Practice;
 
 public class Rectangle extends Area{
 	void area() {
-		System.out.print("Rectangle");
+		System.out.print("Iam a Rectangle");
 	}
 public static void main(String a[]) {
-	Area A=new Area();
-	A.area();
 	Rectangle r=new Rectangle();
 	r.area();
 }
--- ObjectOriented/src/Practice/Square.java
+++ ObjectOriented/src/Practice/Square.java
@@ -1,10 +1,8 @@
 package Practice;
 
 public class Square extends Area{
-    void area() {System.out.println("Square");}
+    void area() {System.out.println("Iam a Square");}
     public static void main(String a[]) {
-    	Area A=new Area();
-    	A.area();
     	Square s=new Square();
     	s.area();
     }
